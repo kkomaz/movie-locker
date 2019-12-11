f70d88143ba375ea73c470516ce4c6ff4430b343
Alex Lee <alex@uniteus.com>
2019-12-11 14:26:00 -0500

    Signed In check

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState, useEffect } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { appConfig } from 'utils/constants'
 import './App.scss';
+import { Container, Navbar, Button } from 'react-bootstrap'
+import { UserSession } from 'blockstack'
 
 function App() {
+  const [userSession] = useState(new UserSession({ appConfig }))
+
+  useEffect(() => {
+    const getHandlePendingSignIn = async () => {
+      return await userSession.handlePendingSignIn()
+    }
+
+    if (!userSession.isUserSignedIn() && userSession.isSignInPending()) {
+      const userData = getHandlePendingSignIn()
+
+      if (!userData.username) {
+        throw new Error('This app requires a username')
+      }
+
+      window.location = '/'
+    }
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar>
+        <Navbar.Brand>Movie Locker</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            {
+              userSession.isUserSignedIn() ?
+              <Button variant="link">Sign Out</Button> :
+              <Button variant="link">Sign In</Button>
+            }
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Navbar>
+      <Container>
+        {
+          userSession.isUserSignedIn() ?
+          <div>Is Signed In</div> :
+          <div>Is Not Signed In</div>
+        }
+      </Container>
     </div>
   );
 }
